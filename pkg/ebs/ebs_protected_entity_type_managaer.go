eafb064a66e33361a1b8c86b539a103aef35f6da
Dave Smith-Uchida <dsmithuchida@vmware.com>
2020-04-06 21:26:23 -0700

    Added a CLI and updates to client

--- pkg/ebs/ebs_protected_entity_type_managaer.go
+++ pkg/ebs/ebs_protected_entity_type_managaer.go
@@ -2,8 +2,11 @@ package ebs
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/sirupsen/logrus"
 	
 	"github.com/vmware-tanzu/astrolabe/pkg/astrolabe"
@@ -18,7 +21,8 @@ func NewEBSProtectedEntityTypeManagerFromConfig(params map[string]interface{}, s
 	session := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String(params["aws-region"].(string)),
 	}))
-	
+
+
 	return &EBSProtectedEntityTypeManager{
 		session: *session,
 	}, nil
@@ -33,7 +37,29 @@ func (this EBSProtectedEntityTypeManager) GetProtectedEntity(ctx context.Context
 }
 
 func (this EBSProtectedEntityTypeManager) GetProtectedEntities(ctx context.Context) ([]astrolabe.ProtectedEntityID, error) {
-	panic("implement me")
+	svc := ec2.New(&this.session)
+	input := &ec2.DescribeVolumesInput{}
+
+	result, err := svc.DescribeVolumes(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return nil, err
+	}
+
+	var returnIDs []astrolabe.ProtectedEntityID
+	for _, curEBS := range result.Volumes {
+		returnIDs = append(returnIDs, astrolabe.NewProtectedEntityID("ebs", *curEBS.VolumeId))
+	}
+	return returnIDs, nil
 }
 
 func (this EBSProtectedEntityTypeManager) Copy(ctx context.Context, pe astrolabe.ProtectedEntity, options astrolabe.CopyCreateOptions) (astrolabe.ProtectedEntity, error) {
