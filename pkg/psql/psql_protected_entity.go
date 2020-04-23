89336d42583f1d370a7854778bf1b103654cbbe3
Dave Smith-Uchida <dsmithuchida@vmware.com>
2020-04-22 23:05:29 -0700

    Added MD

--- pkg/psql/psql_protected_entity.go
+++ pkg/psql/psql_protected_entity.go
@@ -1,13 +1,16 @@
 package psql
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
 	"github.com/vmware-tanzu/astrolabe/pkg/astrolabe"
 	"io"
 	"io/ioutil"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -15,12 +18,12 @@ import (
 )
 
 type PSQLProtectedEntity struct {
-	id astrolabe.ProtectedEntityID
-	petm * PSQLProtectedEntityTypeManager
+	id        astrolabe.ProtectedEntityID
+	petm      *PSQLProtectedEntityTypeManager
 	namespace string
 }
 
-func NewPSQLProtectedEntity(id astrolabe.ProtectedEntityID, petm * PSQLProtectedEntityTypeManager) PSQLProtectedEntity {
+func NewPSQLProtectedEntity(id astrolabe.ProtectedEntityID, petm *PSQLProtectedEntityTypeManager) PSQLProtectedEntity {
 	return PSQLProtectedEntity{
 		id:   id,
 		petm: petm,
@@ -46,16 +49,16 @@ func (this PSQLProtectedEntity) Snapshot(ctx context.Context) (astrolabe.Protect
 	snapshotID := astrolabe.NewProtectedEntitySnapshotID(snapshotUUID.String())
 
 	/*
-	pod, err := this.petm.KubeClient.Pods(this.namespace).Create(&v1.Pod{
-		TypeMeta:   meta_v1.TypeMeta{},
-		ObjectMeta: meta_v1.ObjectMeta{
-			Name: "snapshot-pg-"+snapshotID.String(),
-		},
-		Spec:       v1.PodSpec{},
-		Status:     v1.PodStatus{},
-	})
-	pod.
-	panic("implement me")
+		pod, err := this.petm.KubeClient.Pods(this.namespace).Create(&v1.Pod{
+			TypeMeta:   meta_v1.TypeMeta{},
+			ObjectMeta: meta_v1.ObjectMeta{
+				Name: "snapshot-pg-"+snapshotID.String(),
+			},
+			Spec:       v1.PodSpec{},
+			Status:     v1.PodStatus{},
+		})
+		pod.
+		panic("implement me")
 	*/
 	peSnapshotDir := filepath.Join(this.petm.snapshotsDir, this.id.String())
 	peSnapshotDirInfo, err := os.Stat(peSnapshotDir)
@@ -75,42 +78,44 @@ func (this PSQLProtectedEntity) Snapshot(ctx context.Context) (astrolabe.Protect
 		return astrolabe.ProtectedEntitySnapshotID{}, errors.New(fmt.Sprintf("snapshot directory %s for pe %s is not a directory", peSnapshotDir, this.id.String()))
 	}
 	snapshotPEID := this.id.IDWithSnapshot(snapshotID)
-	snapshotFilename := filenameForSnapshot(snapshotPEID)
-	snapshotPath := filepath.Join(peSnapshotDir, snapshotFilename)
 
-	snapshotFile, err := os.Create(snapshotPath)
+	snapshotMDFilename := mdFilenameForSnapshot(snapshotPEID)
+	snapshotMDPath := filepath.Join(peSnapshotDir, snapshotMDFilename)
+	snapshotMDFile, err := os.Create(snapshotMDPath)
+	if err != nil {
+		return astrolabe.ProtectedEntitySnapshotID{}, errors.Wrapf(err, "Failed to create md file %s", snapshotMDPath)
+	}
 
-	podName := "snapshot-pg-"+snapshotID.String()
-	cmd := exec.Command("/usr/bin/kubectl", "run", "-n", "postgres-test", podName, "--image=dpcpinternal/pg-dump:0.0.5",
-		"--env", "PGPASSWORD=2sw82j65LuzV2oC0ZoXhvtCt53daed3A6QrLyTSa42nXp91Nsyep1KlGw9woJDSs", "--env",
-		"PGHOST=acid-minimal-cluster", "-it")
+	mdReader, err := this.GetMetadataReader(ctx)
+	_, err = io.Copy(snapshotMDFile, mdReader)
 
-	cmdStdout, err := cmd.StdoutPipe()
+	snapshotDataFilename := dataFilenameForSnapshot(snapshotPEID)
+	snapshotDataPath := filepath.Join(peSnapshotDir, snapshotDataFilename)
+
+	snapshotDataFile, err := os.Create(snapshotDataPath)
+	if err != nil {
+		return astrolabe.ProtectedEntitySnapshotID{}, errors.Wrapf(err, "Failed to create data file %s", snapshotDataPath)
+	}
+
+	dataReader, err := this.GetDataReader(ctx)
 	if err != nil {
 		return astrolabe.ProtectedEntitySnapshotID{}, errors.Wrap(err, "Failed to get cmd's stdout")
 	}
 
-	finished := make(chan copyResults)
-	go copyAndNotify(snapshotFile, cmdStdout, finished)
-	err = cmd.Start()
-	if err != nil {
-		return astrolabe.ProtectedEntitySnapshotID{}, errors.Wrap(err, "Failed to start command")
-	}
-	err = cmd.Wait()
+	_, err = io.Copy(snapshotDataFile, dataReader)
+
 	if err != nil {
 		return astrolabe.ProtectedEntitySnapshotID{}, errors.Wrap(err, "Command failed")
 	}
-	results := <- finished
-	if results.err != nil {
-		return astrolabe.ProtectedEntitySnapshotID{}, errors.Wrapf(err, "Copy failed, copied = %d", results.written)
-	}
+
 	return snapshotID, nil
 }
 
 type copyResults struct {
 	written int64
-	err error
+	err     error
 }
+
 func copyAndNotify(dst io.WriteCloser, src io.ReadCloser, finished chan copyResults) {
 	written, err := io.Copy(dst, src)
 	dst.Close()
@@ -121,17 +126,24 @@ func copyAndNotify(dst io.WriteCloser, src io.ReadCloser, finished chan copyResu
 		err:     err,
 	}
 }
-const snapshotExtension = ".psql-snap"
 
-func filenameForSnapshot(snapshotPEID astrolabe.ProtectedEntityID) string {
-		return snapshotPEID.String() + snapshotExtension
+const snapshotDataExtension = ".psql-snap-data"
+
+func dataFilenameForSnapshot(snapshotPEID astrolabe.ProtectedEntityID) string {
+	return snapshotPEID.String() + snapshotDataExtension
+}
+
+const snapshotMDExtension = ".psql-snap-md"
+
+func mdFilenameForSnapshot(snapshotPEID astrolabe.ProtectedEntityID) string {
+	return snapshotPEID.String() + snapshotMDExtension
 }
 
 func snapshotPEIDForFilename(filename string) (astrolabe.ProtectedEntityID, error) {
-	if !strings.HasSuffix(filename, snapshotExtension) {
-		return astrolabe.ProtectedEntityID{}, errors.New(fmt.Sprintf("%s does not end with %s", filename, snapshotExtension))
+	if !strings.HasSuffix(filename, snapshotDataExtension) {
+		return astrolabe.ProtectedEntityID{}, errors.New(fmt.Sprintf("%s does not end with %s", filename, snapshotDataExtension))
 	}
-	peidStr := strings.TrimSuffix(filename, snapshotExtension)
+	peidStr := strings.TrimSuffix(filename, snapshotDataExtension)
 	return astrolabe.NewProtectedEntityIDFromString(peidStr)
 }
 
@@ -157,7 +169,7 @@ func (this PSQLProtectedEntity) ListSnapshots(ctx context.Context) ([]astrolabe.
 	}
 
 	for _, curFileInfo := range fileInfo {
-		if strings.HasSuffix(curFileInfo.Name(), snapshotExtension) {
+		if strings.HasSuffix(curFileInfo.Name(), snapshotDataExtension) {
 			peid, err := snapshotPEIDForFilename(curFileInfo.Name())
 			if err != nil {
 
@@ -187,8 +199,38 @@ func (this PSQLProtectedEntity) GetID() astrolabe.ProtectedEntityID {
 
 func (this PSQLProtectedEntity) GetDataReader(ctx context.Context) (io.ReadCloser, error) {
 	if !this.id.HasSnapshot() {
-		// TODO - go ahead an do a pg dump here directly
-		return nil, errors.New("Cannot read from non-snapshot")
+		psql, err := this.petm.getPostgresqlForPEID(ctx, this.id)
+		if err != nil {
+			return nil, errors.Wrapf(err, "could not retrive psql resource for %s", this.id.String())
+		}
+		namespace := psql.Namespace
+		pghost := psql.ObjectMeta.Name
+		pgsecret, err := this.petm.KubeClient.Secrets(namespace).Get("postgres."+pghost+".credentials", metav1.GetOptions{})
+		if err != nil {
+			return nil, errors.Wrap(err, "could not retrieve secret")
+		}
+		pguser := string(pgsecret.Data["username"])
+		pgpassword := string(pgsecret.Data["password"])
+		fmt.Printf("pguser = %s, pgpassword = %s\n", pguser, pgpassword)
+		dumpUUID, err := uuid.NewRandom()
+		if err != nil {
+			return nil, errors.Wrap(err, "could not create UUID")
+		}
+		podName := "snapshot-pg-" + dumpUUID.String()
+		cmd := exec.Command("/usr/bin/kubectl", "run", "-n", namespace, podName, "--image=dpcpinternal/pg-dump:0.0.5",
+			"--env", "PGPASSWORD=" + pgpassword, "--env",
+			"PGHOST=" + pghost, "--env", "PGUSER=" +pguser, "-it", "--restart=Never", "--rm")
+
+		cmdStdout, err := cmd.StdoutPipe()
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to get cmd's stdout")
+		}
+		err = cmd.Start()
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to start command")
+		}
+
+		return cmdStdout, nil
 	}
 	peSnapshotDir := filepath.Join(this.petm.snapshotsDir, this.id.GetBaseID().String())
 	peSnapshotDirInfo, err := os.Stat(peSnapshotDir)
@@ -198,12 +240,31 @@ func (this PSQLProtectedEntity) GetDataReader(ctx context.Context) (io.ReadClose
 	if !peSnapshotDirInfo.IsDir() {
 		return nil, errors.New(fmt.Sprintf("snapshot directory %s for pe %s is not a directory", peSnapshotDir, this.id.String()))
 	}
-	snapshotFilename := filenameForSnapshot(this.id)
+	snapshotFilename := dataFilenameForSnapshot(this.id)
 	snapshotPath := filepath.Join(peSnapshotDir, snapshotFilename)
 	reader, err := os.Open(snapshotPath)
 	return reader, err
 }
 
 func (this PSQLProtectedEntity) GetMetadataReader(ctx context.Context) (io.ReadCloser, error) {
-	panic("implement me")
+	if !this.id.HasSnapshot() {
+		psql, err := this.petm.getPostgresqlForPEID(ctx, this.id)
+		if err != nil {
+			return nil, errors.Wrapf(err, "could not retrive psql resource for %s", this.id.String())
+		}
+		psqlBytes, err := json.Marshal(psql)
+		return ioutil.NopCloser(bytes.NewReader(psqlBytes)), nil
+	}
+	peSnapshotDir := filepath.Join(this.petm.snapshotsDir, this.id.GetBaseID().String())
+	peSnapshotDirInfo, err := os.Stat(peSnapshotDir)
+	if err != nil {
+		return nil, errors.Wrapf(err, "stat on snapshotsdir %s failed", peSnapshotDir)
+	}
+	if !peSnapshotDirInfo.IsDir() {
+		return nil, errors.New(fmt.Sprintf("snapshot directory %s for pe %s is not a directory", peSnapshotDir, this.id.String()))
+	}
+	snapshotFilename := mdFilenameForSnapshot(this.id)
+	snapshotPath := filepath.Join(peSnapshotDir, snapshotFilename)
+	reader, err := os.Open(snapshotPath)
+	return reader, err
 }
